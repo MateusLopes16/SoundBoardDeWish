deeec6db035274934f1bf52863fc0ac33cd3fbca
Mateus <matlop1604@gmail.com>
2023-05-25 23:26:49 +0200

    boit

--- discordbot.py
+++ discordbot.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import os
+import random
 
 connected = False
 
@@ -14,33 +15,39 @@ client = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.command()
-async def greg(ctx, repeat=1):
-    await play(ctx, 'greg.mp3', repeat)
+commands_names = ["squeezie", "wesh", "cringe", "reuf", "spies", "cha", "kouisine", "spiderman", "libulule", "cerceuil", "donut", "greg", "arab","discours","chant", "monkey", "prout", "ah", "rage"]
 
+for command_name in commands_names:
+    @client.command(name=command_name)
+    async def play_command(ctx, repeat=1, name=command_name):
+        await ctx.message.delete()
+        print(name)
+        await play(ctx, name + '.mp3', repeat)
+    
 @client.command()
-async def spies(ctx, repeat=1):
-    await play(ctx, 'spies.mp3', repeat)
+async def all(ctx):
+    await ctx.message.delete()
+    await ctx.send("""
+**LISTE DE TOUTES LES COMMANDES** 
+\n
+ - 📞 !greg, *(oue c'est greg)*
+ - 🤪 !wesh, *(weeeeesssshhhhhh)*
+ - 🥱 !cringe, *(fin frerot, tu es cringe)*
+ - 😡 !reuf, *(quoi, comment ca mon reuf ?)*
+ - 👀 !spies, *(tu tu tu,   tu tu tu)*
+ - 🤩 !cha, *(cha cha cha cha cha cha)*
+ - 🙍 !kouisine, *(la place de la femme c'est a la cuisine)*
+ - 🕸️ !spiderman, *(chui spiderman fdp)*
+ - 🍺 !libulule, *(je pete ma biere ma libulule)*
+ - ⚰️ !cerceuil, *(musique du confinement)*
+ - 🤷‍♂️ !squeezie, *(est ce que c'est bon pour vous)*
+ - 🥯 !donut, *(donut sucré au sucre)*
+ - !insulte, *(un bot qui insulte random)*
+ - !audio, *(plus qu'a cliquer sur l'emoji)*
 
-@client.command()
-async def gadjet(ctx, repeat=1):
-    await play(ctx, 'gadjet.mp3', repeat)
-
-@client.command()
-async def flop(ctx, repeat=1):
-    await play(ctx, 'flop.mp3', repeat)
-
-@client.command()
-async def beter(ctx, repeat=1):
-    await play(ctx, 'beter.mp3', repeat)
-
-@client.command()
-async def bzez(ctx, repeat=1):
-    await play(ctx, 'bzez.mp3', repeat)
-
-@client.command()
-async def cha(ctx, repeat=1):
-    await play(ctx, 'cha.mp3', repeat)
+*On peut ajouter un chiffre apres la commande pour repeter l'audio plusieurs fois (max 3)*
+*Exemple: !wesh 2*
+""")
 
 #client command erase that erase all the existing messages in the channel
 @client.command()
@@ -49,12 +56,19 @@ async def erase(ctx, amount=100):
 
 async def toomuch(ctx):
     await play(ctx, 'toomuch.mp3', 1)
+
+@client.command()
+async def insulte(ctx):
+    await ctx.message.delete()
+    n = random.randint(1, 5)
+    print(n)
+    file = 'insulte/insulte' + str(n) + '.mp3'
+    await play(ctx, file, 1)
     
 async def play(ctx, mp3_file, repeat):
     global connected
-    if repeat > 10:
+    if repeat > 3:
         if connected:
-            await ctx.message.delete()
             await ctx.send("Ton grand pere qui a fait la guerre ta cru a vie c'était un artchaut, tu t'est prit pour eminem a vouloir débiter autant !")
             return
         await toomuch(ctx)
@@ -72,7 +86,6 @@ async def play(ctx, mp3_file, repeat):
         await voice_channel.disconnect()
         connected = False
     else:
-        await ctx.message.delete()
         await ctx.send("Ton grand daron avec ses tresses de mexicain calme ta joie, j'suis occupé la !")
 
 @client.event
@@ -82,5 +95,56 @@ async def on_command_error(ctx, error):
         await ctx.send("alors deja que ta pas eu ton bac, en plus tu connais pas le nom des commandes ?!")
     else:
         raise error
+    
 
+@client.command()
+async def audio(ctx):
+    await ctx.message.delete()
+    message = await ctx.send("Choose a song to play:")
+    await message.add_reaction("📞")
+    await message.add_reaction("🤪")
+    await message.add_reaction("🥱")
+    await message.add_reaction("😡")
+    await message.add_reaction("👀")
+    await message.add_reaction("🤩")
+    await message.add_reaction("🙍")
+    await message.add_reaction("🕸️")
+    await message.add_reaction("🍺")
+    await message.add_reaction("⚰️")
+    await message.add_reaction("🤷‍♂️")
+    await message.add_reaction("🥯")
+    
+    try:
+        reaction, user = await client.wait_for('reaction_add', timeout=30.0)
+    except TimeoutError:
+        await ctx.send("trop tard fdp")
+        return
+
+    await message.delete()
+    if str(reaction.emoji) == "📞":
+        await play(ctx, 'greg.mp3', 1)
+    elif str(reaction.emoji) == "🤪":
+        await play(ctx, 'wesh.mp3', 1)
+    elif str(reaction.emoji) == "🥱":
+        await play(ctx, 'cringe.mp3', 1)
+    elif str(reaction.emoji) == "😡":
+        await play(ctx, 'reuf.mp3', 1)
+    elif str(reaction.emoji) == "👀":
+        await play(ctx, 'spies.mp3', 1)
+    elif str(reaction.emoji) == "🤩":
+        await play(ctx, 'cha.mp3', 1)
+    elif str(reaction.emoji) == "🙍":
+        await play(ctx, 'kouisine.mp3', 1)
+    elif str(reaction.emoji) == "🕸️":
+        await play(ctx, 'spiderman.mp3', 1)
+    elif str(reaction.emoji) == "🍺":
+        await play(ctx, 'libulule.mp3', 1)
+    elif str(reaction.emoji) == "⚰️":
+        await play(ctx, 'cerceuil.mp3', 1)
+    elif str(reaction.emoji) == "🤷‍♂️":
+        await play(ctx, 'squeezie.mp3', 1)
+    elif str(reaction.emoji) == "🥯":
+        await play(ctx, 'donut.mp3', 1)
+    
+    
 client.run('MTEwNzY3OTU0MzU3NTE4NzUxNg.GkkglV.f-oT8CyiOk48r3tMG4-lHb6VhkN9D93panOoH8')
